9fa20422d39ed3141d1d561d97289b4b24413606
Ashok <n_ashokraju@hotmail.com>
2021-09-11 18:22:39 -0400

    Checkin

--- dist/src/model/Trip.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Trip {
-    constructor(name, start) {
-        this.name = name;
-        this.start = start;
-    }
-}
-exports.default = Trip;
